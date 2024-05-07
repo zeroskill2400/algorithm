dd92e0e574711f047742c9dc84faf9da9f519194
zeroskill <168759695+zeroskill2400@users.noreply.github.com>
2024-05-07 15:06:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)sc.nextLine().charAt(0));
+    }
+}
