1e73c18b94165c3fed45d126903b5c3560547cdc
zeroskill <168759695+zeroskill2400@users.noreply.github.com>
2024-05-07 14:41:13 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String S = scanner.nextLine();
+        int i = scanner.nextInt();
+        System.out.println(S.charAt(i-1));
+    }
+}
