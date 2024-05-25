a435aa3e017659d23358649c12033f1f2567a106
zeroskill <168759695+zeroskill2400@users.noreply.github.com>
2024-05-25 17:31:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 22.63 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,30 +1,13 @@
 class Solution {
     public int[] solution(String s) {
-        Dto dto = new Dto(0, s);
         int[] answer = new int[2];
-        while(!dto.convertedString.equals("1")) {
-            dto = binaryConversion(dto);
+        while(!s.equals("1")) {
+            String convertedString = s.replaceAll("0", "");
+            int zeroCounter = s.length() - convertedString.length();
+            s = Integer.toBinaryString(convertedString.length());
             answer[0]++;
-            answer[1] = dto.zeroCounter;
+            answer[1] += zeroCounter;
         }
         return answer;
-    }
-    public Dto binaryConversion(Dto dto) {
-        int count = dto.zeroCounter;
-        for(int i =0; i < dto.convertedString.length(); i++) {
-            if(dto.convertedString.charAt(i)=='0') {
-                count++;
-            }
-        }
-        return new Dto(count, Integer.toBinaryString(dto.convertedString.replaceAll("0", "").length()));
-    }
-}
-class Dto {
-    int zeroCounter;
-    String convertedString = "";
-
-    Dto(int zeroCounter, String convertedString) {
-        this.zeroCounter = zeroCounter;
-        this.convertedString = convertedString;
-    }
+    }   
 }
